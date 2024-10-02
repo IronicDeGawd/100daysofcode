303acc10aa9ae5d03ef2427a81011a342edeb763
IronicDeGawd <adityasrivastava807@gmail.com>
2024-10-02 23:59:39 +0530

    Day 4 completed, basic react and hooks

---
+++ day4_basicvitereact/src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [ number, setNumber ] = useState(0);
+  function addNumber(){
+    return setNumber(number+1)
+  }
+  function decNumber(){
+    return setNumber(number-1)
+  }
+
+  return (
+    <>
+     <h1>Basic React App</h1>
+     <h3>Counter : {number}</h3>
+     <button onClick={addNumber}>Add Number</button>
+     <button onClick={decNumber}>Decrease Number</button>
+    </>
+  )
+}
+
+export default App
