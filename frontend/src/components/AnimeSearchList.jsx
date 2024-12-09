31b1c3b95eb9a5efd3b2269e50ec3ebb3fa04ec0
IronicDeGawd <adityasrivastava807@gmail.com>
2024-12-09 07:54:43 +0530

    day 18 19 20, working on working on backend integration with different services

--- frontend/src/components/AnimeSearchList.jsx
+++ frontend/src/components/AnimeSearchList.jsx
@@ -5,7 +5,7 @@ export default function AnimeSearchList({ data, index }) {
     <>
       <div
         key={index}
-        className="bg-white rounded-lg overflow-hidden shadow-lg transform hover:scale-105 transition-all duration-300"
+        className="bg-transparent border-2 border-purple-700 rounded-lg overflow-hidden shadow-lg transform hover:scale-105 transition-all duration-300"
       >
         <img
           className="w-full h-48 object-cover"
@@ -13,10 +13,10 @@ export default function AnimeSearchList({ data, index }) {
           alt={data.title}
         />
         <div className="p-4">
-          <h3 className="font-semibold text-lg text-gray-700 truncate">
+          <h3 className="font-semibold text-lg text-gray-200 truncate">
             {data.title}
           </h3>
-          <p className="text-sm text-gray-500">Episodes: {data.episode}</p>
+          <p className="text-sm text-gray-400">Episodes: {data.episode}</p>
           <button className="w-full mt-4 bg-purple-600 text-white py-2 px-4 rounded-md shadow-md hover:bg-purple-700 transition-all">
             <a href={data.link_url} target="_blank" rel="noopener noreferrer">
               Watch Now
