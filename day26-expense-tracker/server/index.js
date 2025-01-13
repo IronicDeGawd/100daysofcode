7d97f21d0771d05608df89e3add4c65db5db7d31
IronicDeGawd <adityasrivastava807@gmail.com>
2025-01-13 23:58:04 +0530

    Learning authentication via cookies and jwt, learnt about types of cookies and importance against localstorage

--- day26-expense-tracker/server/index.js
+++ day26-expense-tracker/server/index.js
@@ -10,16 +10,33 @@ const PORT = 8000;
 //Middleware
 app.use(express.json());
 app.use(cookieParser());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 
 //Test storage
 const users = [];
+const id = 0;
 
 //Routes
 app.get("/", (req, res) => {
   res.send({ key: "pair" });
 });
 
-app.post("/register", (req, res) => {});
+app.post("/register", (req, res) => {
+  try {
+    const email = req.body.email;
+    const password = req.body.password;
+    const hashedPassword = bcrypt.hash(password);
+    const token = jwt.sign({ email: email, userId: id }, SECRET_KEY);
+    res.cookie("_Auth_Token", token, 43200000);
+    users.push({ email: email, password: hashedPassword });
+    id++;
+  } catch (error) {} //token expires in 12 hours
+});
 
 app.get("/login", (req, res) => {});
 
